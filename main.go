f1ed14b178d6f109190cdadb553e39ecfc08b2cf
Akash Goswami <akashgoswami@gmail.com>
2019-09-25 11:39:10 +0000

    reset

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
 import nats "github.com/nats-io/nats.go"
-import "github.com/syndtr/goleveldb/leveldb"
 import "time"
 import "fmt"
 //import "encoding/binary"
+import "github.com/syndtr/goleveldb/leveldb"
+import "github.com/syndtr/goleveldb/leveldb/opt"
+import "github.com/syndtr/goleveldb/leveldb/filter"
+
 
 import (
 	"log"
 	"os"
 	"os/signal"
+	"sync"
 )
 
 
@@ -17,50 +21,103 @@ func main () {
     // Connect to a server
     nc, _ := nats.Connect("nats://172.31.46.240:4222")
     
-    db, _ := leveldb.OpenFile("/tmp/db", nil)
+    o := &opt.Options{
+    	Filter: filter.NewBloomFilter(10),
+    }
+
+    db, err := leveldb.OpenFile("/var/db/state", o)
+    
+    if err != nil {
+         fmt.Println("Unable to open Database ", err)
+         return
+    }
     defer db.Close()
     
-    count  := 10*1000000
-
-    received := 0
-    start := time.Now()
+    chunk := 1000000
     
-    sub, _ :=  nc.QueueSubscribe("test", "result.workers", func(m *nats.Msg) {
+    received := 0
+    missed := 0
+    hit := 0
+    
+    start := time.Now()
+    batch := new(leveldb.Batch)
+    var mutex = &sync.Mutex{}
+
+    ticker := time.NewTicker(1000 * time.Millisecond)
+    done := make(chan bool)
+    go func() {
+        for {
+            select {
+            case <-done:
+                return
+            case t := <-ticker.C:
+                //fmt.Println("Tick at", t)
+                t = t
+                if batch.Len() > 0 {
+                    mutex.Lock()
+                    err = db.Write(batch, nil)
+                    batch.Reset();
+                    //batch = new(leveldb.Batch)
+                    mutex.Unlock()
+                }
+            }
+        }
+    }()
+    
+
+    sub, _ :=  nc.QueueSubscribe("state.>", "state.workers", func(m *nats.Msg) {
         
          received++;
 
          if received == 1 {
             start = time.Now()
          }
+
+        mutex.Lock()
+        batch.Put([]byte(m.Subject) , []byte(m.Data))
+        if received % 1000 == 0{
+            err = db.Write(batch, nil)
+            batch.Reset();
+        }
+        mutex.Unlock()
         
-         db.Put([]byte(fmt.Sprintf("key-%010d", received)) , []byte(m.Data), nil)
-    
-         if received % 100000 == 0{
-             log.Printf("+")
-         }
-         if received == count {
+        if received % chunk == 0{
         	elapsed := time.Since(start)
-            fmt.Println("Set wps", float64(count)/elapsed.Seconds())
-        
-            // Unsubscribe
-            //sub.Unsubscribe()
-            // Drain
-            //sub.Drain()
-            // Close connection
-            nc.Close()
+            fmt.Println("Received last", chunk, "at rate m/s", float64(chunk)/elapsed.Seconds())
+            start = time.Now()
          }
     })
     sub.SetPendingLimits(-1, -1)
+
+    querysub, _ := nc.Subscribe("state.query", func(m *nats.Msg) {
+        //fmt.Println("Received query", m.Data)
+        
+        data, err := db.Get([]byte(m.Data), nil)
+        if err != nil {
+            fmt.Println("Coud not find", string(m.Data))
+            missed++
+        } else {
+            //fmt.Println("Found", string(data))
+            m.Respond(data)
+            nc.Flush();
+            hit++
+        }
+            
+
+
+    })
+    querysub.SetPendingLimits(-1, -1)
 
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	<-c
 	log.Println()
+	ticker.Stop();
 	log.Printf("Draining...")
 	nc.Drain()
 	log.Fatalf("Exiting")
-	
+
 
 }
 
